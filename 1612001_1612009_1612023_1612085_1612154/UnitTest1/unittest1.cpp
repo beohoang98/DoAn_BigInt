95dd7016316ebb674b4d9a0b649a92726b3c3d89
Beo Hoang <beohoang98@users.noreply.github.com>
2018-03-22 12:45:03 +0700

    adding to unit test

--- 1612001_1612009_1612023_1612085_1612154/UnitTest1/unittest1.cpp
+++ 1612001_1612009_1612023_1612085_1612154/UnitTest1/unittest1.cpp
@@ -9,13 +9,36 @@ namespace UnitTest1
 	TEST_CLASS(Test1)
 	{
 	public:
-
-		TEST_METHOD(TestKhoiTao) {
+		///Test Khoi tao
+		TEST_METHOD(TestKhoiTao_DEC) {
+			BigInt A("10050", 10);
+			Assert::AreEqual("10050", A.toString().c_str());
+		}
+		TEST_METHOD(TestKhoiTao_HEX) {
 			BigInt A("FFAADD", 16);
-
 			Assert::AreEqual("FFAADD", A.toString(16).c_str());
 		}
+		TEST_METHOD(TestKhoiTao_BIN) {
+			BigInt A("01001100", 2);
+			Assert::AreEqual("01001100", A.toString(2).c_str());
+		}
+
+		///Test Chuyen doi co so
+		TEST_METHOD(Test_HEX_TO_BIN) {
+			BigInt A("F0FF", 16);
+			Assert::AreEqual("1111000011111111", A.toString(2).c_str());
+		}
+		TEST_METHOD(Test_BIN_TO_HEX) {
+			BigInt A("10101111", 2);
+			Assert::AreEqual("AF", A.toString(16).c_str());
+		}
+		TEST_METHOD(Test_DEC_TO_BIN) {
+			BigInt A("15", 10);
+			Assert::AreEqual("1111", A.toString(2).c_str());
+		}
 		
+
+		///Test phep toan
 		TEST_METHOD(TestCongSoNho)
 		{
 			BigInt A("101010", 2);
