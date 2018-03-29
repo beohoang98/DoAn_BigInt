f8228944176653d2b7d9a62ad42046a6ff33dab1
Beo Hoang <beohoang98@users.noreply.github.com>
2018-03-29 17:13:33 +0700

    add again phep chia

--- 1612001_1612009_1612023_1612085_1612154/Test_O_Day/main.cpp
+++ 1612001_1612009_1612023_1612085_1612154/Test_O_Day/main.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 #include "../1612001_1612009_1612023_1612085_1612154/BigInt.h"
 
-
 int main() {
-	BigInt A("255");
-	BigInt C = A << 12;
-	
-	cout << C.toString() << endl;
+
 	system("pause");
 	return 0;
 }
